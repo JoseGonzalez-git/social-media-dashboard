2b16f9c0362f44c284f0f96b71e86e184f56c399
Jose Gonzalez <JoseGonzalez-git>
2022-04-12 20:54:30 -0500

    Change: Add functionaliti into the cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
-import FollowersCard from './components/followersCard';
-import Header from './components/header';
+import React from "react";
+import User from "./utils/user.json";
+import FollowersCard from "./components/followersCard";
+import Header from "./components/header";
 
+let FollowersToDay = (value: number[]) => {
+  let a = value[0];
+  let b = value[1];
+  let subtraction = a - b;
+  return subtraction;
+};
 function App() {
   return (
     <div className="App">
@@ -9,10 +16,22 @@ function App() {
         <Header />
       </div>
       <div className="Body container">
-        <FollowersCard/>
-        <FollowersCard/>
-        <FollowersCard/>
-        <FollowersCard/>
+        {Object.values(User).map((socialNetwork, i) => {
+          let followersDay: number[] = Object.values(
+            socialNetwork.TotalFollowers
+          );
+          return (
+            <FollowersCard
+              key={i}
+              user={socialNetwork.user}
+              totalFollowers={Object.values(socialNetwork.TotalFollowers)
+                .map(Number)
+                .reduce((a, b) => a + b, 0)}
+              typeFollow={socialNetwork.typeFollow}
+              calculatedFollowersByDays={FollowersToDay(followersDay)}
+            />
+          );
+        })}
       </div>
     </div>
   );
--- src/components/followersCard.tsx
+++ src/components/followersCard.tsx
@@ -1,26 +1,48 @@
-import IconFacebook from '../assets/icon-facebook.svg';
-import IconUp from '../assets/icon-up.svg';
-import '../styles/followersCard.sass';
+import "../styles/followersCard.sass";
+import IconFacebook from "../assets/icon-facebook.svg";
+import IconUp from "../assets/icon-up.svg";
+import IconDown from "../assets/icon-down.svg";
 
-let FollowersCard = ()=> {
-    return (
-        <>
-            <div className="container-card card-border">
-                <div className="card-header">
-                <img src={IconFacebook} alt={'Facebook Icon'}/>
-                    <p> @Josegonzalez</p>
-                </div>
-                <div className="card-body">
-                    <h1>1999</h1>
-                    <p>Followers</p>
-                </div>
-                <div className="card-footer">
-                    <img src={IconUp} alt={'Up'}/>
-                    <p>12 Today</p>
-                </div>
-            </div>
-        </>
-    )
+interface PropFollowersCard {
+  user: string;
+  totalFollowers: number;
+  typeFollow: string;
+  calculatedFollowersByDays: number;
 }
+
+let isUpOrDown = (value: number) => {
+  if (value < 0) {
+    return [IconDown, "Down"];
+  } else {
+    return [IconUp, "Up"];
+  }
+};
+
+let FollowersCard = ({
+  user,
+  totalFollowers,
+  typeFollow,
+  calculatedFollowersByDays,
+}: PropFollowersCard) => {
+  let Icon: string[] = isUpOrDown(calculatedFollowersByDays);
+  return (
+    <>
+      <div className="container-card card-border">
+        <div className="card-header">
+          <img src={IconFacebook} alt={"Facebook Icon"} />
+          <p> {user}</p>
+        </div>
+        <div className="card-body">
+          <h1>{totalFollowers}</h1>
+          <p>{typeFollow}</p>
+        </div>
+        <div className="card-footer">
+          <img src={Icon[0]} alt={Icon[1]} />
+          <p>{calculatedFollowersByDays * -1} Today</p>
+        </div>
+      </div>
+    </>
+		);
+};
 
 export default FollowersCard;
